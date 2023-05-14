6d77a1d82f6be39cbdcaeba41adf3f75bddae228
Erick Alcântara Silvério <ericksilverio39@gmail.com>
2023-05-14 15:49:42 -0300

    Add files via upload

---
+++ app/validacao.js
@@ -0,0 +1,45 @@
+function verificaSeOChutePossuiValorValido(chute) {
+    const numero = +chute
+
+    if (chuteForInvalido(numero)) {
+        elementoChute.innerHTML += '<div>Valor inválido</div>'
+    }
+
+    if (numeroForMaiorOuMenorQueOValorPermitido(numero)) {
+        elementoChute.innerHTML += `
+        <div>Valor inválido: fale um número entre ${menorValor} e ${maiorValor}</div>`
+    }
+
+    if (numero === numeroSecreto) {
+        document.body.innerHTML = `
+        <h2>Parabéns, você acertou!</h2>
+        <h3>O número secreto era ${numeroSecreto}!</h3>
+        <button id="jogar-novamente" class="btn-jogar">Jogar novamente</button>`
+    }
+
+    else if (numero > numeroSecreto) {
+        elementoChute.innerHTML += `
+        <div>O número secreto é menor <i class="fa-solid fa-arrow-down"></i></div>
+        `
+    }
+
+    else {
+        elementoChute.innerHTML += `
+        <div>O número secreto é maior <i class="fa-solid fa-arrow-up"></i></div>
+        `
+    }
+}
+
+function numeroForMaiorOuMenorQueOValorPermitido(numero) {
+    return numero > maiorValor || numero < menorValor
+}
+
+function chuteForInvalido(numero) {
+    return Number.isNaN(numero)
+}
+
+document.body.addEventListener('click', e => {
+    if (e.target.id == 'jogar-novamente') {
+        window.location.reload()
+    }
+})
